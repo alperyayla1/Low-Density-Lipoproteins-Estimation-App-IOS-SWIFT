bd70e38f9ae9300498533cef0ccf99b1ed8728f9
capybara <alperyayla2023@gmail.com>
2024-09-09 00:08:56 +0300

    Initial Commit

---
+++ capy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  capy
+//
+//  Created by Deniz Coşkun on 9.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ capy/capyApp.swift
@@ -0,0 +1,17 @@
+//
+//  capyApp.swift
+//  capy
+//
+//  Created by Deniz Coşkun on 9.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct capyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
